ed353d55bafb4227f8c5e69544ea52f2bb789af4
nagae-memooff <169192@gmail.com>
2014-09-17 18:43:39 +0800

    改进登陆行为，增加判断

--- common/login_spec.rb
+++ common/login_spec.rb
@@ -24,23 +24,29 @@ class Login
   end
 end
 
+# TODO:设定登陆方式：邮箱/工号/login_name
+
+
 
 describe "用户" do
   login = Login.getInstance
   it "管理员登陆" do
-    super_admin = User.new({:login_name=>'admin@minxing',:password=>'111111'}).login
+    super_admin = User.new({:login_name=>'admin@ee.com',:password=>'111111'}).login
     login.super_admin = super_admin
+#     puts super_admin.inspect
+    expect(super_admin.errors).to be_nil, "管理员登陆失败，#{super_admin.inspect}"
     
   end
   
   describe do
     it "获取用户列表,并登录" do
     #let(:users) do
-      response = get '/api/v1/users.json', {:page=>1,:limit=>4},super_admin.header
-      response[:items].each do |user|
+      user_infos = get('/api/v1/users.json', {:page=>1,:limit=>4}, super_admin.header)[:items][1..-1]
+      user_infos.each do |user|
         if user[:name] != '管理员' then
-          user = User.new({:login_name=> user[:name] + '@minxing.com',:password=>'111111'}).login
+          user = User.new({:login_name=> user[:pinyin] + '@ee.com',:password=>'111111'}).login
           users << user
+          expect(user.errors).to be_nil, "登陆失败， #{user.inspect}"
         end
       end
       login.users = users
